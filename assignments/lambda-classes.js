51550c01cfa80c0a88de9b2ef7f5a8cc27c854e4
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-16 13:49:49 -0700

    Team Working

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+class Person {
+    constructor(baseClass){
+        this.name = baseClass.name;
+        this.age = baseClass.age;
+        this.location = baseClass.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+console.log(fred.name);
