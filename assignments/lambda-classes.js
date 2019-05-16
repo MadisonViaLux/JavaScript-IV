98bbd1a30188f1a44e0cf33cd3cef486c0ba2f1a
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-16 15:23:04 -0700

    team work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,43 +1,74 @@
 // CODE here for your Lambda Classes
 
 
-// class Person===================================================
+ //class Person===================================================
 class Person {
     constructor(baseClass){
         this.name = baseClass.name;
         this.age = baseClass.age;
         this.location = baseClass.location;
-    }
-    speak(){
-        return `Hello my name is ${this.name}, I am from ${this.location}.`;
-    }
-}
+     }
 
-//  CLASSES instuctor ==========================================
+     speak(){
+     return `Hello my name is ${this.name}, I am from ${this.location}`;
+     }
 
-class Instructor {
-    constructor(baseInst) {
-        super(baseInst)
+    }
+
+ //  CLASSES instuctor ==========================================
+
+ class Instructor extends Person {
+     constructor(baseInst) {
+         super(baseInst)
         
-        this.specialty = baseInst.specialty;
-        this.favLanguage = baseInst.favLanguage;
-        this.catchPhrase = baseInst.catchPhrase;
-    }
-    demo(subject){
+         this.specialty = baseInst.specialty;
+         this.favLanguage = baseInst.favLanguage;
+         this.catchPhrase = baseInst.catchPhrase;
+     }
+    demo(subject) {
         return `Today we are learning about ${subject}`;
-    }
+     }
     grade(student, subject) {
-        `${student.name} receives a perfect score on ${subject}`
-    }
-}
+        return `${student.name} receives a perfect score on ${subject}`;
+     }
+  }
 
-class Student extends Person{
-    constructor(baseStudent) {
-        super(baseStudent)
+
+//   Class student========================================================
+ class Student extends Person {
+     constructor(baseStudent) {
+         super(baseStudent)
         
-        this.previousBackground = baseStudent.previousBackground;
-        this.className = baseStudent.className;
-        this.favSubjects = baseStudent.favSubjects;
+         this.previousBackground = baseStudent.previousBackground;
+         this.className = baseStudent.className;
+         this.favSubjects = baseStudent.favSubjects;
+         
+     }
+     listsSubjects() {
+     return `${this.favSubjects}`;
+     }
+     PRAssignment(subject) {
+     return `${student.name} has submitted a PR for ${subject}`;
+     }
+     sprintChallenge() {
+     return `${student.name} has begun sprint challenge on ${subject}`;
+     }
+ }
+
+// Project Manager ==================================================
+
+class ProjectManager extends Instructor {
+    constructor(basePM) {
+        super(basePM)
+
+        this.gradClassName = basePM.gradClassName;
+        this.favInstuctor = basePM.favInstuctor;
+    }
+    standUp(channel) {
+        return `${name} announces to ${channel}, at channel standy times!`;
+    }
+    debugsCode(student, subject) {
+         return `${name} debugs ${student.name}'s code on ${subject}`;
     }
 }
 
@@ -47,32 +78,43 @@ class Student extends Person{
 
 
 
+ const fred = new Instructor({
+     name: 'Fred',
+     location: 'Bedrock',
+     age: 37,
+     favLanguage: 'JavaScript',
+     specialty: 'Front-end',
+     catchPhrase: `Don't forget the homies`
+ });
 
 
+ const bob = new Student({
+    name: 'Bob',
+    previousBackground: 'Elf',
+    className: 'Larp20',
+    favSubjects: 'Larping, Html, CSS',
+    subject: 'redux'
+ });
+
+const jack = new ProjectManager({
+    name: 'Jack',
+    gradClassName: 'Larp19',
+    favInstuctor: 'Dan Shermnmnm',
+})
 
 
-
-
-
-
-
-// const fred = new Instructor({
-//     name: 'Fred',
-//     location: 'Bedrock',
-//     age: 37,
-//     favLanguage: 'JavaScript',
-//     specialty: 'Front-end',
-//     catchPhrase: `Don't forget the homies`
-// });
-
-
-
-let dog = "woof";
 
 
 
 
 // CONSOLE LOGS =================================================
 
-// console.log(fred.location);
-console.log(dog);
+ console.log(fred.location);
+ console.log(jack.gradClassName);
+ console.log(bob.className);
+ console.log(bob.sprintChallenge);
+ console.log(bob.speak());
+
+
+ 
+
