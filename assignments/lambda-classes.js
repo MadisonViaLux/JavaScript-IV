d1b79ffeffb9edc67db3081d01b5dd34733fc2ff
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-16 14:36:39 -0700

    tech down

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,14 +1,7 @@
 // CODE here for your Lambda Classes
 
-const fred = new Instructor({
-    name: 'Fred',
-    location: 'Bedrock',
-    age: 37,
-    favLanguage: 'JavaScript',
-    specialty: 'Front-end',
-    catchPhrase: `Don't forget the homies`
-  });
 
+// class Person===================================================
 class Person {
     constructor(baseClass){
         this.name = baseClass.name;
@@ -20,4 +13,66 @@ class Person {
     }
 }
 
-console.log(fred.name);
+//  CLASSES instuctor ==========================================
+
+class Instructor {
+    constructor(baseInst) {
+        super(baseInst)
+        
+        this.specialty = baseInst.specialty;
+        this.favLanguage = baseInst.favLanguage;
+        this.catchPhrase = baseInst.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person{
+    constructor(baseStudent) {
+        super(baseStudent)
+        
+        this.previousBackground = baseStudent.previousBackground;
+        this.className = baseStudent.className;
+        this.favSubjects = baseStudent.favSubjects;
+    }
+}
+
+
+// CONSTS =========================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fred = new Instructor({
+//     name: 'Fred',
+//     location: 'Bedrock',
+//     age: 37,
+//     favLanguage: 'JavaScript',
+//     specialty: 'Front-end',
+//     catchPhrase: `Don't forget the homies`
+// });
+
+
+
+let dog = "woof";
+
+
+
+
+// CONSOLE LOGS =================================================
+
+// console.log(fred.location);
+console.log(dog);
