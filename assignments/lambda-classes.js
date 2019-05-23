0c4de9c8e90428946a6acac93610ecf1f2595a18
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-23 14:00:56 -0700

    progress

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,15 +1,53 @@
 // CODE here for your Lambda Classes
 
 
-// Class Person============================================================
+// Person=================================================================
 
-// CLASSES instuctor ====================================================
+class Person{
+    constructor(personObj){
+        this.personName = personObj.name;
+        this.personAge = personObj.age;
+        this.personLocation = personObj.location;
+    }
+    speak(){
+        return `Hello my name is ${personName}, I am from ${personLocation}!`;
+    }
+}
 
-// Class student========================================================
+
+// Instuctor =============================================================
+
+class Instructor extends Person{
+    constructor(InstObj){
+        super(InstObj);
+        this.InstSpec = InstObj.specialty;
+        this.InstLang = InstObj.favLanguage;
+        this.InstPhrase = InstObj.catchPhrase;
+    };
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`)
+    };
+
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    };
+}
+
+
+// Student================================================================
+
+
+
 
 // Project Manager =======================================================
 
+
+
+
 // CONSTS ================================================================
+
+
+
 
 // CONSOLE LOGS ==========================================================
 
